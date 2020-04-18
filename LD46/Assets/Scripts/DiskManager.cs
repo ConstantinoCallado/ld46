116cab1fd65ae8d8eda5a2da97b5e367f441f63e
constantinoc <constantinoc@gamehouse.com>
2020-04-18 20:01:02 +0200

    Added disk manager and player interaction

---
+++ LD46/Assets/Scripts/DiskManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiskManager : MonoBehaviour
+{
+    public static DiskManager diskManagerRef;
+    public List<Disk> playList;
+    private List<Disk> diskPool = new List<Disk>();
+    public int refreshDiskDelay = 2;
+    public int amountDisksInDeck = 3;
+    public List<Disk> disksInDeck = new List<Disk>();
+    public List<Transform> diskAnchors = new List<Transform>();
+
+    void Awake()
+    {
+        diskManagerRef = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        diskPool = new List<Disk>(playList);
+        disksInDeck = new List<Disk>(amountDisksInDeck);
+
+        for (int i = 0; i < amountDisksInDeck; i++)
+        {
+            SpawnDiskAtDeck(i);
+        }
+    }
+
+    public void SpawnDiskAtDeck(int position)
+    {
+        Disk instantiatedDisk = InstantiateDiskFromList();
+        if (position > disksInDeck.Count - 1)
+        {
+            disksInDeck.Insert(position, instantiatedDisk);
+        }
+        else
+        {
+            disksInDeck[position] = instantiatedDisk;
+        }
+
+        instantiatedDisk.transform.parent = diskAnchors[position].transform;
+        instantiatedDisk.transform.localPosition = Vector3.zero;
+        instantiatedDisk.transform.localRotation = Quaternion.identity;
+        diskAnchors[position].gameObject.SetActive(false);
+    }
+
+    private Disk InstantiateDiskFromList()
+    {
+        if(diskPool.Count == 0)
+        {
+            diskPool = new List<Disk>(playList);
+        }
+
+        Disk result;
+        int randomIndex = Random.Range(0, diskPool.Count);
+        result = diskPool[randomIndex];
+        diskPool.RemoveAt(randomIndex);
+
+        return GameObject.Instantiate(result, transform.position, Quaternion.identity);
+    }
+
+    public void UseDisk(Disk disk)
+    {
+        int position = disksInDeck.IndexOf(disk);
+        disksInDeck[position] = null;
+        StartCoroutine(RefillDiskAtPosition(position));
+    }
+
+    private IEnumerator RefillDiskAtPosition(int position)
+    {
+        yield return new WaitForSeconds(refreshDiskDelay);
+        SpawnDiskAtDeck(position);
+    }
+
+    public void ShowDisksInDeck(bool show = true)
+    {
+        for (int i = 0; i < amountDisksInDeck; i++)
+        {
+            if (disksInDeck[i] != null)
+            {
+                diskAnchors[i].gameObject.SetActive(show);
+            }
+        }
+    }
+}
---
+++ LD46/Assets/Scripts/Hero.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    public Camera mainCamera;
+    private Plate lastPlateClicked = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject objectHit = hit.transform.gameObject;
+
+                Debug.Log("Clicked " + objectHit.name);
+
+                // Check the behaviour depending on the object clicked
+
+                // If player clicked a disk after clicking a plate
+                if (objectHit.GetComponent<Disk>() != null && lastPlateClicked != null)
+                {
+                    // Equip disk!
+                    Disk clickedDisk = objectHit.GetComponent<Disk>();
+                    lastPlateClicked.EquipDisk(clickedDisk);
+                    lastPlateClicked = null;
+                    DiskManager.diskManagerRef.UseDisk(clickedDisk);
+                }
+
+                // Player clicked a plate!
+                else if (lastPlateClicked == null && objectHit.GetComponent<Plate>() != null)
+                {
+                    lastPlateClicked = objectHit.GetComponent<Plate>();
+                    DiskManager.diskManagerRef.ShowDisksInDeck(true);
+                }
+
+                /*
+                // Player clicked somewhere else
+                else
+                {
+                    lastPlateClicked = null;
+                }
+                */
+                // Hide the deck
+                if(!lastPlateClicked)
+                {
+                    DiskManager.diskManagerRef.ShowDisksInDeck(false);
+                }
+            }
+        }
+    }
+}
