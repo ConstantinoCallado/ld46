c9a7131634e8c911e63b7dbea65f00d0b2067133
constantinoc <constantinoc@gamehouse.com>
2020-04-18 12:52:12 +0200

    Added test scene

---
+++ LD46/Assets/Scripts/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+        StartCoroutine(JumpRandom());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator JumpRandom()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(2f, 4f));
+            rigidBody.AddForce(new Vector3(0, 2, 0), ForceMode.VelocityChange);
+        }
+    }
+}
