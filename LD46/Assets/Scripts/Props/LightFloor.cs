171edaf5d8a32910dd27c137d2a925d0cac2cc52
constantinoc <constantinoc@gamehouse.com>
2020-04-19 22:30:07 +0200

    Added juicy effects to some props

---
+++ LD46/Assets/Scripts/Props/LightFloor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFloor : MonoBehaviour
+{
+    public float randomizeFloorDelay = 5;
+    public List<Material> tileMaterials = new List<Material>();
+    public MeshRenderer floorRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RandomizeFloor());
+    }
+
+    IEnumerator RandomizeFloor()
+    {
+        while(true)
+        {
+            List<Material> materialPool = new List<Material>(tileMaterials);
+            Material[] newMaterials = new Material[4];
+
+            for(int i=0; i<newMaterials.Length; i++)
+            {
+                int randomIndex = Random.Range(0, materialPool.Count);
+
+                newMaterials[i] = materialPool[randomIndex];
+
+                materialPool.RemoveAt(randomIndex);
+            }
+
+            floorRenderer.materials = newMaterials;
+
+            yield return new WaitForSeconds(randomizeFloorDelay);
+        }
+    }
+}
